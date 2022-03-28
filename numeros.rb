a6e55addc43d4ae45abf5c871f703617222b3c1a
Rkanraeden <lecortezc@gmail.com>
2022-03-28 18:01:05 -0300

    finished commit:numeros.rb

--- numeros.rb
+++ numeros.rb
@@ -10,14 +10,19 @@
 # 12345
 
 num = ARGV[0].to_i
+
+# num.times do |i|
+#     (i+1).times do |j|
+#     print (i%5)+1
+#     end
+#     print "\n"
+# end
+
 num.times do |i|
-    if i.even? 
-    puts
-    print "\n"
-    else
-    
+    base = 1
+    (i+1).times do |j|
+        print base
+        base += 1
     end
-
-
+    print "\n"
 end
-
--- patrones.rb
+++ patrones.rb
@@ -1,14 +1,16 @@
-# linea de arriba
+# Escribir un programa llamado patrones.rb con métodos que reciban la cantidad de líneas y
+# muestren por pantalla los siguientes patrones de *:
 
-# cover debe darnos num "*", por lo que se usará para arriba y abajo.
+#letra o
+#cover debe darnos num"*" por lo que se usará para arriba y para abajo.
 def cover(num)
-    num.times do
-        print ="* "
-    print "\n"
+    num.times do 
+        print "* " 
     end
+    print "\n"
 end
 
-#middle nos dará "* " num " " "*" la razón por la que necesitamos 3 espacios vacíos
+# middle nos dará "*" num " " "*" 
 def middle(num)
     print "* "
     (num-2).times do |i|
@@ -17,9 +19,7 @@ def middle(num)
     print "* "
     print "\n"
 end
-
-# linea de abajo
-def letra_o(n)
+def letra_o(num)
     cover(num)
     (num-2).times do |i|
         middle(num)
@@ -29,4 +29,109 @@ end
 
 letra_o(5)
 
+print "\n"
 
+# Inicio letra I
+
+# *****
+#   *
+#   *
+#   *
+# *****
+
+# Vamos a reutilizar cover
+
+def middle_i(num)
+    (num-2).times do |i|
+        num.times do |j|
+            # if (j > num/2) || (j < num/2)
+            #     print "  "
+            # else
+            #     print "* "
+            # end 
+            if j== num/2
+                print"* "
+            else 
+                print "  "
+            end
+        end
+        print "\n"
+    end
+end
+
+def letra_i(num)
+    cover(num)
+    middle_i(num)
+    cover(num)
+end
+
+letra_i(5)
+
+print "\n"
+
+# 0 0 1 0 0 i == 0 j == 2 j > num/2 || j < num/2
+# 0 0 1 0 0 i == 1 j == 2 j == num/2
+# 0 0 1 0 0 i == 2 j == 2
+
+# d. método letra_x(n)
+# n = 5
+
+# 1 0 0 0 1 i == 0 j == 0 || i == 0 j == 4
+# 0 1 0 1 0 i == 1 j == 1 || i == 1 j == 3
+# 0 0 1 0 0 i == 2 j == 2 || i == 2 j == 2
+# 0 1 0 1 0 i == 3 j == 3 || i == 3 j == 1
+# 1 0 0 0 1 i == 4 j == 4 || i == 4 j == 0 
+
+def letra_x(n)
+    n.times do |i|
+        n.times do |j|
+            if i==j || j== n-1-i
+                print"* "
+            else
+                print"  "
+            end
+        end
+        print "\n"
+    end
+end
+
+letra_x(5)
+
+print "\n"
+# numero cero 
+# n = 5
+
+
+
+# 1 1 1 1 1
+# 1 1 0 0 1
+# 1 0 1 0 1
+# 1 0 0 1 1
+# 1 1 1 1 1 
+
+#cover debe darnos num"*" por lo que se usará para arriba y para abajo.
+def cover(num)
+    num.times do 
+        print "* " 
+    end
+    print "\n"
+end
+
+# middle nos dará "*" num " " "*" 
+def middle(num)
+    print "* "
+    (num-2).times do |i|
+        print "  "
+    end
+    print "* "
+    print "\n"
+end
+def numero_0(num)
+    cover(num)
+    (num-2).times do |i|
+        middle(num)
+    end
+    cover(num)
+end
+
+numero_0(5)
